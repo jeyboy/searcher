3b1bd3e0b1a690c79e6a5d498f1b53c8a2dae583
JB <jeyboy@bigmir.net>
2012-09-28 02:16:00 +0300

    narafet

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -3,6 +3,6 @@ module CategoriesHelper
   include ActionView::Helpers::TextHelper
 
   def prepare_list(topics)
-    topics.map { |topic| "<li>#{link_to "#{topic.name} (#{pluralize(topic.posts_count, 'post')})", Rails.application.routes.url_helpers.topic_path(topic)}</li>" }
+    topics.map { |topic| "<li class='container'>#{link_to "#{topic.name} (#{pluralize(topic.posts_count, 'post')})", Rails.application.routes.url_helpers.topic_path(topic)}</li>" }
   end
 end
