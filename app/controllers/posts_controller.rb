894dfddd8daed5fd10f99d026ac2deda2c972221
JB <jeyboy@bigmir.net>
2012-09-28 23:36:50 +0300

    add post edit. fix post form javascript

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,26 @@
 class PostsController < ApplicationController
   before_filter :preinit, :only => [:related, :destroy, :edit, :update, :show, :delete_tag]
-  before_filter -> {@show_topic = true}, :only => [:relevant, :related, :index_by_tags]
+  before_filter :init_vars, :only => [:new, :edit]
 
   def show
   end
 
   def new
     @post = Post.new
-    @categories = Category.all
-    @tags = Post.all_tags
   end
 
   def edit
+    category = @post.topic.category
+    @category_id = category.id
+    @topics = category.topics
+    render :action => :new
   end
 
   def create
     topic = Topic.find_by_id(params[:post].delete(:topic_id))
     (redirect_to(topics_path, :alert => "Topic not find") and return) unless topic
     @post = topic.posts.build(params[:post])
-    @post.tag_list = params[:tag].map(&:last)
+    @post.tag_list = params[:tag].map(&:last) unless params[:tag].blank?
     if (@post.save)
       redirect_to topic_path(@post.topic_id)
     else
@@ -27,7 +29,11 @@ class PostsController < ApplicationController
   end
 
   def update
-    if @post.update_attributes(params[:post])
+    @post.topic_id = params[:post].delete(:topic_id)
+    @post.attributes.merge!(params[:post])
+    @post.tag_list << params[:tag].map(&:last) unless params[:tag].blank?
+
+    if @post.save
       redirect_to topic_path(@post.topic_id)
     else
       render :action => :edit
@@ -64,6 +70,11 @@ class PostsController < ApplicationController
   end
 
 protected
+  def init_vars
+    @categories = Category.all
+    @tags = Post.all_tags
+  end
+
   def preinit
     @post = Post.find_by_id(params[:id])
     redirect_to :root, :alert => "Access denied" if @post.nil?
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 module ApplicationHelper
   include ActsAsTaggableOn::TagsHelper
+
+  def prepare_form_params(obj)
+    {
+        url: "/#{obj.class.name.pluralize.downcase}/#{obj.id.to_s}",
+        html: {method: obj.id.blank? ? :post : :put, :class => "#{obj.class.name.downcase}_form"}
+    }
+  end
 end
