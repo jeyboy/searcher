e0f3c02c7928716abf26faf2c9c5183a3fbff2ea
Jenua Boiko <jeyboy@bigmir.net>
2012-10-01 16:21:38 +0300

    fixes

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,7 @@ class Category < ActiveRecord::Base
   has_many :topics, :dependent => :destroy
 
   validates :name, :presence => true, :length => { :within => 1..512 }, :uniqueness => true
+  before_save :prepare_avatar
 
   def self.init_rails_cache
     Category.includes(:topics).all.each_with_object({}) do |cat, ret|
@@ -13,5 +14,10 @@ class Category < ActiveRecord::Base
 
   def self.cached_topics(category_id)
     (Rails.cache.read(:categories_tree) || init_rails_cache)[category_id]
+  end
+
+protected
+  def prepare_avatar
+    self.avatar ||= "/assets/category_default_small.png"
   end
 end
