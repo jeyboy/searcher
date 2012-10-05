b9a1e20100fde409a8024aa00c8e8adf923c5498
JB <jeyboy@bigmir.net>
2012-10-06 01:49:33 +0300

    fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
 protected
-  def paging(obj)
-    obj.page(params[:page] || 1)
+  def paging(obj, per_page = nil)
+    obj.page(params[:page] || 1).per(per_page || Kaminari.config.config.default_per_page)
   end
 end
