4e70858822b84cf511ee858bb2cef0f9809503bd
JB <jeyboy@bigmir.net>
2012-10-11 20:43:21 +0300

    clean

--- app/assets/javascripts/libraries/icontains.js
+++ app/assets/javascripts/libraries/icontains.js
@@ -1,3 +1,7 @@
+//example
+//  if (select.find(':icontains("' + text + '")').length == 0)
+//    select.append('<option value="' + text + '">' + text + '</option>');
+
 $.expr[':'].icontains = function(obj, index, meta, stack){
     return (obj.textContent || obj.innerText || jQuery(obj).text() || '').toLowerCase().indexOf(meta[3].toLowerCase()) >= 0;
 };
--- app/assets/javascripts/main/inputs.js
+++
@@ -1,7 +0,0 @@
-//= require ../libraries/icontains.js
-
-function appendOptionIfNotExist(select, text) {
-    select = $(select);
-    if (select.find(':icontains("' + text + '")').length == 0)
-        select.append('<option value="' + text + '">' + text + '</option>');
-}
