eef682cd8fd418688a4eeb5201fa8e6136e2531f
Jenua Boiko <jeyboy1985@gmail.com>
2013-10-20 00:24:59 +0400

    improve tag model

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,6 +5,15 @@ class TagsController < ApplicationController
     render json: {status: @tag.save, id: @tag.id, text: @tag.name}
   end
 
+  def posts
+    @tag = Tag.where(params[:id]).first
+    if @tag
+      @posts = @tag.posts
+    else
+      redirect_to :back, alert: 'Tag not found'
+    end
+  end
+
   private
     def tag_params
       params.require(:tag).permit(:name)
--- app/models/taggable.rb
+++ app/models/taggable.rb
@@ -3,4 +3,6 @@ class Taggable < ActiveRecord::Base
   belongs_to :tag
 
   validates :tag_id, uniqueness: {scope: :tagged_id}
+
+  scope :for_posts, -> { tagged_type == 'Post' }
 end
