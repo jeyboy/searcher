77eebb58e046eee9cb558b2dad9ad1fe6f2b9aa6
Jenua Boiko <jeyboy1985@gmail.com>
2013-11-04 04:31:49 +0400

    add postgree for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,20 @@ Searcher2::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'example.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'example.com',
+      authentication: 'plain',
+      enable_starttls_auto: true,
+      user_name: ENV['GMAIL_USERNAME'],
+      password: ENV['GMAIL_PASSWORD']
+  }
 end
