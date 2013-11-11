5a59c20698ebc82bff6bb4c7b27a84a5ce5de24d
Jenua Boiko <jeyboy1985@gmail.com>
2013-11-11 05:56:31 +0400

    link posts with user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
   def edit;  end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.new(post_params)
 
     respond_to do |format|
       if @post.save
