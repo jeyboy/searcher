3a3aab0fb03b760b06619d6c3c8711b9323ae3eb
Jenua Boiko <jeyboy1985@gmail.com>
2013-12-14 08:47:47 +0400

    update

--- app/controllers/dbs_controller.rb
+++ app/controllers/dbs_controller.rb
@@ -2,7 +2,7 @@ require 'dumper'
 
 class DbsController < ApplicationController
   def create
-    `rake db:seed:dump EXCLUDE=[]`
+    `rake db:seed:dump EXCLUDE=[created_at, updated_at]`
     send_file Rails.root.join('db', 'seeds.rb')
     #::Dumper.dump(ActiveRecord::Base.connection) unless File.exist?(::Dumper.file_path)
     #send_file(::Dumper.file_path)
